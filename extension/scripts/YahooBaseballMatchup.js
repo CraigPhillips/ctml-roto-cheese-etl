c6d7465018085b579a8368ccb29780cf583dae10
Craig Phillips <Craig.Phillips@FrozenExports.net>
2014-03-03 22:02:45 -0800

    Added new class for looking up and storing matchup data.

---
+++ extension/scripts/YahooBaseballMatchup.js
@@ -0,0 +1,8 @@
+/*
+	Represents a single weekly matchup between two fantasy baseball teams on Yahoo's fantasy baseball site.
+	
+	matchupUrl - The URL of the page which represents this matchup.
+*/
+var YahooBaseballMatchup = YahooBaseballCallbackObject.extend(function(matchupUrl)) {
+	
+});
