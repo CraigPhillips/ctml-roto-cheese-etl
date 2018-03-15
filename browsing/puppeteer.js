b5f4ce0ebfc315ed16523d405d685478639812f7
Craig Phillips <craigap@uw.edu>
2018-03-15 15:54:10 -0700

    finished scraping of pieces needed to calculate roto scores

--- browsing/puppeteer.js
+++ browsing/puppeteer.js
@@ -8,6 +8,7 @@ class ActionTypes {
     this.enterText = Symbol('fe-cheese-puppeteer-enter-text');
     this.click = Symbol('fe-cheese-puppeteer-click');
     this.getAtts = Symbol('fe-cheese-puppeteer-get-attributes');
+    this.getText = Symbol('fe-cheese-puppeteer-get-text');
   }
 };
 const actionType = new ActionTypes();
@@ -39,16 +40,19 @@ class Puppeteer {
       for (let i of toDos.keys()) {
         const action = toDos[i];
         const errorPre = `action ${i + 1}`;
+        const valuesSubset = [];
         if (!action.type) throw new Error(`${errorPre} missing type`);
 
         switch(action.type)
         {
           case actionType.browseTo:
             if (!action.url) throw new Error(`${errorPre} missing URL`);
+
             await page.goto(action.url);
             break;
           case actionType.click:
             if (!action.field) throw new Error(`${errorPre} missing field`);
+
             await page.waitForSelector(action.field);
             const clickTarget = await page.$(action.field);
             await clickTarget.click();
@@ -56,29 +60,39 @@ class Puppeteer {
           case actionType.enterText:
             if (!action.field) throw new Error(`${errorPre} missing field`);
             if (!action.value) throw new Error(`${errorPre} missing value`);
+
             await page.waitForSelector(action.field);
             const textTarget = await page.$(action.field);
             await textTarget.type(action.value);
             break;
           case actionType.getAtts:
+          case actionType.getText:
             if (!action.field) throw new Error(`${errorPre} missing field`);
-            if (!action.att) throw new Error(`${errorPre} missing attribute`);
+            if (action.type === actionType.getAtts && !action.att) {
+              throw new Error(`${errorPre} missing attribute`);
+            }
 
             await page.waitForSelector(action.field);
             const selectedFields = await page.$$(action.field);
             for (const field of selectedFields) {
-              const value = await page.evaluate((el, att) => {
-                return el.getAttribute(att);
-              }, field, action.att);
-              values.push(value);
+              const value = action.type === actionType.getAtts ?
+                await page.evaluate((el, att) => {
+                  return el.getAttribute(att);
+                }, field, action.att) :
+                await page.evaluate((el) => {
+                  return el.textContent;
+                }, field);
+              valuesSubset.push(value? value.trim() : value);
             }
             break;
           default:
-            throw new Error(`unknown action type: ${actionTypeToTake}`);
+            throw new Error(`unknown action type: ${action.type.toString()}`);
         }
+
+        if (valuesSubset.length > 0 ) values.push(valuesSubset);
       }
 
-      return values.length > 1 ? values : values.length ? values[1] : undefined;
+      return values.length === 1? values[0] : values;
     } catch(actionError) {
       throw new VError(actionError, 'error while executing actions');
     }
--- yahoo/fantasy-baseball-league.js
+++ yahoo/fantasy-baseball-league.js
@@ -1,15 +1,15 @@
 const _ = require('privatize')();
 const { actionType, Puppeteer } = require('../browsing/puppeteer');
 
-const { browseTo, click, enterText, getAtts } = actionType;
-const yahooLeagueUrlPrefix = 'https://baseball.fantasysports.yahoo.com/league/';
+const { browseTo, click, enterText, getAtts, getText } = actionType;
+const yahooLeagueUrlPrefix = 'https://baseball.fantasysports.yahoo.com';
 
 class League {
   constructor(leagueName, browser = new Puppeteer()) {
     if (!leagueName) throw new Error('missing league name');
 
     _(this).browser = browser;
-    _(this).leagueName = leagueName;
+    _(this).league = leagueName;
     _(this).password = process.env.FE_CHEESE_YAHOO_PASS;
     _(this).userName = process.env.FE_CHEESE_YAHOO_USER;
   }
@@ -17,9 +17,11 @@ class League {
   async dispose() { _(this).browser.dispose(); }
 
   async getCurrentWeeklyScores() {
+    const leagueSuffix = `/league/${_(this).league}`;
+
     const weeklyMatchups = await _(this).browser
       .do([
-        { type: browseTo, url: `${yahooLeagueUrlPrefix}${_(this).leagueName}` },
+        { type: browseTo, url: `${yahooLeagueUrlPrefix}${leagueSuffix}` },
         { type: enterText, field: '#login-username', value: _(this).userName },
         { type: click, field: '#login-signin' },
         { type: enterText, field: '#login-passwd', value: _(this).password },
@@ -32,6 +34,22 @@ class League {
       ]);
 
     console.log(weeklyMatchups);
+
+    for (let matchup of weeklyMatchups) {
+      // regex: matches any number following mid{X}= where {X} is one digit
+      const teamNumbers = matchup.match(/(?<=mid[\d]=)[\d]+/g);
+      const [categories, scores] = await _(this).browser
+        .do([
+          { type: browseTo, url: `${yahooLeagueUrlPrefix}${matchup}` },
+          { type: getText, field: '#matchup-wall-header th' },
+          { type: getText, field: '#matchup-wall-header td' },
+        ]);
+
+      console.log(teamNumbers);
+      console.log(categories);
+      console.log(scores);
+      console.log();
+    }
   }
 }
 
