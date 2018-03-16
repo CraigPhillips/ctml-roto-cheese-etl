8c69aa2280c4a265358fdeeec7939b22d2693c99
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-03-15 21:37:13 -0700

    added some basic score parsing

--- yahoo/fantasy-baseball-league.js
+++ yahoo/fantasy-baseball-league.js
@@ -33,23 +33,42 @@ class League {
         },
       ]);
 
-    console.log(weeklyMatchups);
-
+    const matchupStatuses = [];
     for (let matchup of weeklyMatchups) {
       // regex: matches any number following mid{X}= where {X} is one digit
       const teamNumbers = matchup.match(/(?<=mid[\d]=)[\d]+/g);
-      const [categories, scores] = await _(this).browser
-        .do([
-          { type: browseTo, url: `${yahooLeagueUrlPrefix}${matchup}` },
-          { type: getText, field: '#matchup-wall-header th' },
-          { type: getText, field: '#matchup-wall-header td' },
-        ]);
-
-      console.log(teamNumbers);
-      console.log(categories);
-      console.log(scores);
-      console.log();
+      matchupStatuses.push({
+        teamNumbers,
+        readAttempt: _(this).browser
+          .do([
+            { type: browseTo, url: `${yahooLeagueUrlPrefix}${matchup}` },
+            { type: getText, field: '#matchup-wall-header th' },
+            { type: getText, field: '#matchup-wall-header td' },
+          ])
+      });
     }
+
+    const results = await Promise.all(matchupStatuses.map(s => s.readAttempt));
+    const scores = {};
+    for (let i of results.keys()) {
+      const teams = matchupStatuses[i].teamNumbers;
+      const [categories, values] = results[i];
+      const team1Score = {};
+      const team2Score = {};
+      for (let catI of categories.keys()) {
+        const cat = categories[catI].toLowerCase().replace('*', '');
+        const team1Value = values[catI] === '-' ? 0 : values[catI];
+        const catI2 = catI + categories.length;
+        const team2Value = values[catI2] === '-' ? 0 : values[catI2];
+        team1Score[cat] = parseFloat(team1Value);
+        team2Score[cat] = parseFloat(team2Value);
+      }
+
+      scores[teams[0]] = team1Score;
+      scores[teams[1]] = team2Score;
+    }
+
+    console.log(scores);
   }
 }
 
